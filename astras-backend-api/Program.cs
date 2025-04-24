77c8e5ba9b7ec3d4cd6eac9e47f1e31598b6859f
Reno Satya Adrian <renos.adrian@gmail.com>
2025-04-24 09:31:17 +0700

    fix con string

--- astras-backend-api/Program.cs
+++ astras-backend-api/Program.cs
@@ -4,7 +4,7 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("Default")));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddControllers();
 
